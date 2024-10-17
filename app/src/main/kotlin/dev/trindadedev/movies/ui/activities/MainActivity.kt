8e46c35cf5175f9bf1cf15304a9aef70baf159bd
trindadedev13 <devsuay@gmail.com>
2024-10-17 14:42:21 -0300

    fix: Locals not provided

--- app/src/main/kotlin/dev/trindadedev/movies/ui/activities/MainActivity.kt
+++ app/src/main/kotlin/dev/trindadedev/movies/ui/activities/MainActivity.kt
@@ -10,10 +10,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.navigation.compose.rememberNavController
 import androidx.compose.ui.Modifier
 
 import dev.trindadedev.movies.navigation.MainNavHost
 import dev.trindadedev.movies.ui.theme.MoviesAppTheme
+import dev.trindadedev.movies.platform.LocalMainNavController
 
 class MainActivity : ComponentActivity() {
 
@@ -26,9 +29,21 @@ class MainActivity : ComponentActivity() {
                      modifier = Modifier.fillMaxSize(),
                      color = MaterialTheme.colorScheme.background
                 ) {
-                    MainNavHost()
+                    ProvideMainCompositionLocals { 
+                        MainNavHost() 
+                    }
                 }
             }
         }
+    }
+    
+    @Composable
+    private fun ProvideMainCompositionLocals(content: @Composable () -> Unit) {
+        val navController = rememberNavController()
+
+        CompositionLocalProvider(
+            LocalMainNavController provides navController, 
+            content = content
+        )
     }
 }
