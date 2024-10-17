cd8f2f35985fcfb183c1688cbffce036cbc664d6
trindadedev13 <devsuay@gmail.com>
2024-10-17 16:07:12 -0300

    fix: stringResource not found

--- app/src/main/kotlin/dev/trindadedev/movies/ui/screens/home/HomeScreen.kt
+++ app/src/main/kotlin/dev/trindadedev/movies/ui/screens/home/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.res.*
 import androidx.compose.ui.layout.*
 
 import dev.trindadedev.movies.Strings
