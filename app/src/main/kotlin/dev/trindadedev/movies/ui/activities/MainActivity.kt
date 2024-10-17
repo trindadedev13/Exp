31b27a723d07a211a5f48da5540266013f6697a1
trindadedev13 <devsuay@gmail.com>
2024-10-17 14:18:55 -0300

    fix: Tooling not found

--- app/src/main/kotlin/dev/trindadedev/movies/ui/activities/MainActivity.kt
+++ app/src/main/kotlin/dev/trindadedev/movies/ui/activities/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 
 import dev.trindadedev.movies.navigation.MainNavHost
 import dev.trindadedev.movies.ui.theme.MoviesAppTheme
