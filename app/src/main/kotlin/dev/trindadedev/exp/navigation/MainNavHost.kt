9f8f6332a3b3dc925306807bbc371a20d2a4505b
trindadedev13 <devsuay@gmail.com>
2024-10-22 12:26:26 -0300

    feat: logs in click of project

--- app/src/main/kotlin/dev/trindadedev/exp/navigation/MainNavHost.kt
+++ app/src/main/kotlin/dev/trindadedev/exp/navigation/MainNavHost.kt
@@ -1,5 +1,7 @@
 package dev.trindadedev.exp.navigation
 
+import android.util.Log
+
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -39,7 +41,8 @@ fun MainNavHost() {
         composable<HomeRoute> {
             HomeScreen(
                 onProjectClicked = { project ->
-                    navController.navigate(ProjectDetailsRoute(project))
+                    navController.navigate(ProjectDetailsRoute(project = project))
+                    Log.d("Project", project.toString())
                 }
             )
         }
