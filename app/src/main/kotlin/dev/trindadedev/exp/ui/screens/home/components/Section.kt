b113394c5f56f1360be862ab652681725c954144
trindadedev13 <devsuay@gmail.com>
2024-10-22 03:13:51 -0300

    fix: package androidx.compose.icons not exists

--- app/src/main/kotlin/dev/trindadedev/exp/ui/screens/home/components/Section.kt
+++ app/src/main/kotlin/dev/trindadedev/exp/ui/screens/home/components/Section.kt
@@ -3,8 +3,8 @@ package dev.trindadedev.exp.ui.screens.home.components
 import androidx.compose.foundation.*
 import androidx.compose.foundation.shape.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.icons.*
-import androidx.compose.icons.filled.*
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
